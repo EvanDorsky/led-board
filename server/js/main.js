da80b2ce11fe26708d2d090e93acdd81662ad3b4
Evan Dorsky <evan.dorsky@icloud.com>
2015-09-14 01:41:55 -0400

    Make event happen on button click

--- server/js/main.js
+++ server/js/main.js
@@ -1,15 +1,17 @@
-var req = new XMLHttpRequest()
-req.open('GET', 'https://agent.electricimp.com/5ZWRjqrJHOjn', true)
+function colorImp() {
+    var req = new XMLHttpRequest()
+    req.open('GET', 'https://agent.electricimp.com/5ZWRjqrJHOjn', true)
 
-req.onload = function() {
-    if (this.status >= 200 && this.status < 400) {
-        var data = JSON.parse(this.response);
-    }
-    else {
+    req.onload = function() {
+        if (this.status >= 200 && this.status < 400) {
+            var data = JSON.parse(this.response);
+        }
+        else {
 
+        }
     }
+
+    req.onerror = function() {}
+
+    req.send()
 }
-
-req.onerror = function() {}
-
-req.send()
