03d3a48a3f7cc423316efa2ca625ad204f7b1db4
Evan Dorsky <evan.dorsky@icloud.com>
2015-09-14 01:40:50 -0400

    Add server for imp control

---
+++ server/js/main.js
@@ -0,0 +1,15 @@
+var req = new XMLHttpRequest()
+req.open('GET', 'https://agent.electricimp.com/5ZWRjqrJHOjn', true)
+
+req.onload = function() {
+    if (this.status >= 200 && this.status < 400) {
+        var data = JSON.parse(this.response);
+    }
+    else {
+
+    }
+}
+
+req.onerror = function() {}
+
+req.send()
